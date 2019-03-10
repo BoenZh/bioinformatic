1142fb6a6bde4929a91fb19fa45d6eb76164f681
zhangbb <zhangboen1994@gmail.com>
2019-03-10 17:38:55 +0800

    commit

---
+++ Example/check.py
@@ -0,0 +1,16 @@
+from modeller.automodel import *    # Load the automodel class
+
+log.verbose()    # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files (directory to find 1SDMA.atm)
+env.io.atom_files_directory = './:../atom_files'
+
+a = automodel(env,
+              alnfile  = 'check.pir',     # alignment filename
+              knowns   = '5HAF',              #id codes of the templates, used to find template file
+              sequence = '5ubw')              # id code of the target (or quey)
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 1                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual homology modelling
